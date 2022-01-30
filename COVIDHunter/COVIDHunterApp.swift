1c1bdf875db0bfa3670cf8c9e9ca8edc54090113
Steven Yu <stevenyuser@gmail.com>
2022-01-29 20:13:58 -0500

    Initial Commit

---
+++ COVIDHunter/COVIDHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVIDHunterApp.swift
+//  COVIDHunter
+//
+//  Created by Steven Yu on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct COVIDHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVIDHunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVIDHunter
+//
+//  Created by Steven Yu on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
